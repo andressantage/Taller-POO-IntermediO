cbabc088d58ea96468ecc9337a8db637d0d0a7aa
andressantage <ingeniero.andres.santana@gmail.com>
2023-05-25 11:13:47 -0500

    feat: :sparkles: Puntos del 1 al 5
    A continuar

--- main.js
+++ main.js
@@ -1,4 +1,134 @@
 let nombre
+let fuerza
+let personaje1
+document.getElementById('miFormulario').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    nombre = document.getElementById('nombre').value;
+    fuerza = document.getElementById('fuerza').value;
+    personaje1=new Personaje(nombre,fuerza);
+    document.getElementById("features").textContent="Resultado: "+personaje1.presentarse()
+    console.log(personaje1.presentarse())
+});
+class Personaje{
+    constructor(nombre,fuerza){
+        this.nombre=nombre;
+        this.fuerza=fuerza;
+    }
+    presentarse(){
+        return `El nombre del personajes es ${this.nombre} y su nivel de fuerza es ${this.fuerza}` 
+    }
+}
+
+class Jedi extends Personaje{
+    constructor(nombre,fuerza){
+        super(nombre,fuerza)
+    }
+    usarFuerza(){
+        return `El Jedi esta usando la fuerza para proteger la galaxia` 
+    }
+    entrenar(){
+        this.fuerza=parseFloat(this.fuerza)+10
+        return this.fuerza
+    }
+}
+let nombreJedi
+let fuerzaJedi
+let jedi1
+document.getElementById('miFormulario1').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    nombreJedi = document.getElementById('nombreJedi').value;
+    fuerzaJedi = document.getElementById('fuerzaJedi').value;
+    jedi1=new Jedi(nombreJedi,fuerzaJedi);
+    document.getElementById("Jedi").innerHTML=`
+    <h1>Resultado usarFuerza: ${jedi1.usarFuerza()}</h1>
+    <h1>Resultado entrenar: ${jedi1.entrenar()}</h1>
+    `
+});
+
+class Sith extends Personaje{
+    constructor(nombre,fuerza){
+        super(nombre,fuerza)
+    }
+    usarFuerza(){
+        return `El Sith esta usando la fuerza para conquistar la galaxia` 
+    }
+    corromper(){
+        this.fuerza=parseFloat(this.fuerza)-5
+        return this.fuerza
+    }
+}
+let nombreSith
+let fuerzaSith
+let sith1
+document.getElementById('miFormulario2').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    nombreSith = document.getElementById('nombreSith').value;
+    fuerzaSith = document.getElementById('fuerzaSith').value;
+    sith1=new Sith(nombreSith,fuerzaSith);
+    document.getElementById("Sith").innerHTML=`
+    <h1>Resultado usarFuerza: ${sith1.usarFuerza()}</h1>
+    <h1>Resultado entrenar: ${sith1.corromper()}</h1>
+    `
+});
+
+class MaestroJedi extends Jedi{
+    constructor(nombre,fuerza){
+        super(nombre,fuerza)
+    }
+    ensenar(){
+        this.fuerza=parseFloat(this.fuerza)+20
+        return this.fuerza
+    }
+}
+let yoda
+let nombreYoda
+let fuerzaYoda
+document.getElementById('miFormulario3').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    nombreYoda = document.getElementById('nombreYoda').value;
+    fuerzaYoda = document.getElementById('fuerzaYoda').value;
+    yoda=new MaestroJedi(nombreYoda,fuerzaYoda);
+    document.getElementById("Yoda").innerHTML=`
+    <h1>Resultado presentarse: ${yoda.presentarse()}</h1>
+    <h1>Resultado usarFuerza: ${yoda.usarFuerza()}</h1>
+    <h1>Resultado entrenar: ${yoda.ensenar()}</h1>
+    `
+});
+
+
+let darthVader
+let nombreDarthVader
+let fuerzaDarthVader
+document.getElementById('miFormulario4').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    nombreDarthVader = document.getElementById('nombreDarthVader').value;
+    fuerzaDarthVader = document.getElementById('fuerzaDarthVader').value;
+    darthVader=new Sith(nombreDarthVader,fuerzaDarthVader);
+    document.getElementById("DarthVader").innerHTML=`
+    <h1>Resultado presentarse: ${darthVader.presentarse()}</h1>
+    <h1>Resultado usarFuerza: ${darthVader.usarFuerza()}</h1>
+    <h1>Resultado corromper: ${darthVader.corromper()}</h1>
+    `
+});
+
+let obj1=new Personaje("Jose","1200")
+let obj2=new Personaje("Mario","1500")
+function batalla(obj1,obj2){//hacer aqui un formulario donde se escriba a los dos y luego un boton de enfrentar para que se ataquen
+    if(obj2.fuerza>obj1.fuerza){//falta lo de usar fuerza
+        return `Gano ${obj2.nombre}`
+    }else if(obj2.fuerz===obj1.fuerza){
+        return `La batalla genero un empate`
+    }else{
+        return `Gano ${obj1.nombre}`
+    }
+}
+
+
+
+
+
+//////////////////////////////////////////////////////////////
+/* let nombre
 let edad
 let sexo
 let persona1
@@ -285,4 +415,4 @@ document.getElementById('boton').addEventListener("click", function() {
     empleado1=new Empleado(nombreEmpleado,edadEmpleado,sueldoEmpleado) //id=1
     empleado2=new Empleado(nombreEmpleado,edadEmpleado,sueldoEmpleado) //id=2
     document.getElementById("llamar").innerHTML="<h1>"+ Empleado.generarIdEmpleado() +"</h1>"
-});
+}); */
